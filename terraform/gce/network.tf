a66e993c04a7dc2f3741a2b8af03a722dc947c18
marc <marc@cockroachlabs.com>
2015-11-30 08:10:19 -0500

    make GCE work

--- terraform/gce/network.tf
+++ terraform/gce/network.tf
@@ -1,6 +1,43 @@
-resource "google_compute_http_health_check" "default" {
+resource "google_compute_backend_service" "foobar" {
+    name = "blablah"
+    description = "Hello World 1234"
+    port_name = "http"
+    protocol = "HTTP"
+    timeout_sec = 10
+    region = "us-central1"
+
+    backend {
+        group = "${google_compute_instance_group_manager.cockroach.instance_group}"
+    }
+
+    health_checks = ["${google_compute_http_health_check.cockroach.self_link}"]
+}
+
+resource "google_compute_instance_group_manager" "cockroach" {
+    name = "terraform-test"
+    instance_template = "${google_compute_instance_template.foobar.self_link}"
+    base_instance_name = "foobar"
+    zone = "us-central1-f"
+    target_size = 1
+}
+
+resource "google_compute_instance_template" "cockroach" {
+  name = "cockroach"
+  machine_type = "n1-standard-1"
+
+  network_interface {
+    network = "default"
+  }
+
+  disk {
+    source_image = "${var.gce_image}"
+    auto_delete = true
+    boot = true
+  }
+}
+resource "google_compute_http_health_check" "cockroach" {
   name = "cockroach-health-check"
-  request_path = "/"
+  request_path = "/health"
   port = "${var.cockroach_port}"
   check_interval_sec = 2
   healthy_threshold = 2
@@ -8,21 +45,21 @@ resource "google_compute_http_health_check" "default" {
   timeout_sec = 2
 }
 
-resource "google_compute_target_pool" "default" {
+resource "google_compute_target_pool" "cockroach" {
   name = "cockroach-target-pool"
   # Note: when there are no instances, aws_instance.cockroach.*.id has an empty
   # element, causing failed elb updates. See: https://github.com/hashicorp/terraform/issues/3581
   instances = ["${compact(split(",", join(",",google_compute_instance.cockroach.*.self_link)))}"]
-  health_checks = ["${google_compute_http_health_check.default.name}"]
+  health_checks = ["${google_compute_http_health_check.cockroach.name}"]
 }
 
-resource "google_compute_forwarding_rule" "default" {
+resource "google_compute_forwarding_rule" "cockroach" {
   name = "cockroach-forwarding-rule"
-  target = "${google_compute_target_pool.default.self_link}"
+  target = "${google_compute_target_pool.cockroach.self_link}"
   port_range = "${var.cockroach_port}"
 }
 
-resource "google_compute_firewall" "default" {
+resource "google_compute_firewall" "cockroach" {
   name = "cockroach-firewall"
   network = "default"
 
