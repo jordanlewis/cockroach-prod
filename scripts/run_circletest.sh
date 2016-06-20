c07222e8419827b47928f0632fb54ebf798a26bd
Jordan Lewis <jordanthelewis@gmail.com>
2016-06-20 18:03:04 -0400

    Run acceptance for 1 hour, not 1 minute.
    Previously we were running for 1 minute to test the CircleCI setup. Now
    we should be running for 1 hour as normal.

--- scripts/run_circletest.sh
+++ scripts/run_circletest.sh
@@ -11,7 +11,8 @@ $(download_binary "cockroach/acceptance.test")
 
 LOGS_DIR="${CIRCLE_ARTIFACTS}"
 
-./acceptance.test -test.v -test.run FiveNodesAndWriter -test.timeout 24h -remote -nodes 1 -d 1m -key-name google_compute_engine -l $LOGS_DIR -cwd "${HOME}/cockroach/cloud/gce" > >(tee "${LOGS_DIR}/test.stdout.txt") 2> >(tee "${LOGS_DIR}/test.stderr.txt" >&2)
+./acceptance.test -test.v -test.run FiveNodesAndWriter -test.timeout 24h -remote -nodes 1 -d 1h -key-name google_compute_engine -l $LOGS_DIR -cwd "${HOME}/cockroach/cloud/gce" > >(tee "${LOGS_DIR}/test.stdout.txt") 2> "${LOGS_DIR}/test.stderr.txt"
+
 # trick go2xunit - go test binaries won't print the package summary for some reason
 echo 'ok github.com/cockroachdb/cockroach/acceptance 10.000s' >> "${LOGS_DIR}/test.stdout.txt"
 mkdir -p ${CIRCLE_TEST_REPORTS}/acceptance
